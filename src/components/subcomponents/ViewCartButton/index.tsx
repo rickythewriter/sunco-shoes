d8c8e8295bd09520659d4670478aa7e46ed5480f
Ricky Thang <ricky.writer.dev@gmail.com>
2023-07-28 00:11:44 -0700

    feat - view cart button component - add cart quantity indicator

--- src/components/subcomponents/ViewCartButton/index.tsx
+++ src/components/subcomponents/ViewCartButton/index.tsx
@@ -1,11 +1,17 @@
 import "./ViewCartButton.css"
 import cart from '../../../assets/icons/cart.svg'
+import { useContext } from "react"
+import { ShoppingCartContext, ShoppingCartContextType } from "../../../contexts/ShoppingCartContext"
 
 export default function ViewCartButton() {
+
+    const { productsInCart } = useContext(ShoppingCartContext) as ShoppingCartContextType;
+
     return (
         <div id="view-cart">
             <img id="cart-icon" src={cart} />
             View Cart
+            { productsInCart.length > 0 && <div id="cart-item-quantity">{productsInCart.length}</div> }
         </div>
     )
 }
