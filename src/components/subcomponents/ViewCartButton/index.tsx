06f501322576440d6d7cfceb4dd63f992398af6f
Ricky Thang <ricky.writer.dev@gmail.com>
2023-07-28 07:37:35 -0700

    feat - header - link view cart button to cart

--- src/components/subcomponents/ViewCartButton/index.tsx
+++ src/components/subcomponents/ViewCartButton/index.tsx
@@ -1,6 +1,7 @@
 import "./ViewCartButton.css"
 import cart from '../../../assets/icons/cart.svg'
 import { useContext } from "react"
+import { Link } from "react-router-dom"
 import { ShoppingCartContext, ShoppingCartContextType } from "../../../contexts/ShoppingCartContext"
 
 export default function ViewCartButton() {
@@ -8,10 +9,12 @@ export default function ViewCartButton() {
     const { productsInCart } = useContext(ShoppingCartContext) as ShoppingCartContextType;
 
     return (
-        <div id="view-cart">
-            <img id="cart-icon" src={cart} />
-            View Cart
-            { productsInCart.length > 0 && <div id="cart-item-quantity">{productsInCart.length}</div> }
-        </div>
+        <Link to='/cart'>
+            <div id="view-cart">
+                <img id="cart-icon" src={cart} />
+                View Cart
+                { productsInCart.length > 0 && <div id="cart-item-quantity">{productsInCart.length}</div> }
+            </div>
+        </Link>
     )
 }
